9041db0bf75b2833940e9016168a49c5383008fa
bojjasrirajesh <bojjarhcl123@gmail.com>
2020-02-19 11:32:09 +0000

    initial commit

---
+++ src/main/java/com/test/BasicstructureApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicstructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicstructureApplication.class, args);
+	}
+
+}
